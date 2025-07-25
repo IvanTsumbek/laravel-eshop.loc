d93bbfd5d9a98ae23c6754739e909a0c506cebce
IvanTsumbek <maincumbek@gmail.com>
2025-07-25 20:22:59 +0300

    16.-Сидирование-данных-для-фильтров-

--- app/Models/Filter.php
+++ app/Models/Filter.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\FilterGroup;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Filter extends Model
 {
-    //
+    protected $table = 'filters';
+    protected $fillable = ['title', 'filter_group_id'];
+    public $timestamps = false;
+
+    public function group(): HasOne
+    {
+        return $this->hasOne(FilterGroup::class, 'id', 'filter_group_id');
+    }
 }
