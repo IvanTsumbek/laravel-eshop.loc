7397067dfe172d4fd4429940ffe4d1d268dc0d05
IvanTsumbek <maincumbek@gmail.com>
2025-08-06 21:48:19 +0300

    33.-Оформление-заказа.-Часть-2-

--- app/Livewire/Cart/CheckoutComponent.php
+++ app/Livewire/Cart/CheckoutComponent.php
@@ -2,10 +2,62 @@
 
 namespace App\Livewire\Cart;
 
+use App\Models\Order;
 use Livewire\Component;
+use App\Helpers\Cart\Cart;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class CheckoutComponent extends Component
 {
+    public string $name;
+    public string $email;
+    public string $note;
+
+    public function mount()
+    {
+        $this->name = auth()->user()->name ?? '';
+        $this->email = auth()->user()->email ?? '';
+    }
+
+    public function saveOrder()
+    {
+        $validated = $this->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'note' => 'string|nullable',
+        ]);
+
+        $validated = array_merge($validated, [
+            'user_id' => auth()->id(),
+            'total' => Cart::getCartTotal(),
+        ]);
+
+        try {
+            DB::beginTransaction();
+            $order = Order::create($validated);
+            $order_products = [];
+            $cart = Cart::getCart();
+            foreach ($cart as $product_id => $product) {
+                $order_products[] = [
+                    'product_id' => $product_id,
+                    'title' => $product['title'],
+                    'price' => $product['price'],
+                    'quantity' => $product['quantity'],
+                    'image' => $product['image'],
+                    'slug' => $product['slug'],
+                ];
+            }
+            $order->orderProducts()->createMany($order_products);
+            $this->js("toastr.success('Success ordering!')");
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e->getMessage());
+            $this->js("toastr.error('Error ordering!')");
+        }
+    }
+
     public function render()
     {
         return view('livewire.cart.checkout-component');
