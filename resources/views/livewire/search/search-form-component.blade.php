544712ac7df70dc65df8a4c3c6680e6ff9513e3d
IvanTsumbek <maincumbek@gmail.com>
2025-08-12 19:55:30 +0300

    42.-Поиск.-Часть-2-

--- resources/views/livewire/search/search-form-component.blade.php
+++ resources/views/livewire/search/search-form-component.blade.php
@@ -1,23 +1,29 @@
 <div class="col-sm-6 mt-2 mt-md-0">
     <div class="search-form">
 
-        <form>
+        <form wire:submit="search">
             <div class="input-group">
-                <input type="text" class="form-control" name="s" placeholder="Searching..."
+                <input type="text" class="form-control" wire:model.live.debounce.500ms="term" placeholder="Searching..."
                     aria-label="Searching..." aria-describedby="button-search">
-                <button class="btn btn-outline-warning" type="submit" id="button-search">
+                <button class="btn btn-outline-warning @if (!$term) disabled @endif" type="submit"
+                    id="button-search">
                     <i class="fa-solid fa-magnifying-glass"></i>
                 </button>
             </div>
 
-            <span class="search-empty"><i class="fa-solid fa-xmark"></i></span>
+            @if ($term)
+                <span class="search-empty" x-on:click="$wire.term = ''; $wire.$refresh()">
+                    <i class="fa-solid fa-xmark"></i>
+                </span>
+            @endif
         </form>
 
+        @if (count($search_results))
         <ul class="search-results">
-            <li><a href="#"><span>Product 1</span><span>$100</span></a></li>
-            <li><a href="#"><span>Product 1</span><span>$100</span></a></li>
-            <li><a href="#"><span>Product 1</span><span>$100</span></a></li>
-        </ul>
-
+        @foreach ($search_results as $product)
+        <li><a wire:navigate href="{{ route('product', $product->slug) }}"><span>{{$product->title}}</span><span>${{$product->price}}</span></a></li>
+        @endforeach
+            </ul>
+        @endif
     </div>
 </div>
