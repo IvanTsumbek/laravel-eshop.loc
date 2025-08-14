6091a5901529c43ee14cf9f0f99b565ed6098624
IvanTsumbek <maincumbek@gmail.com>
2025-08-14 12:13:11 +0300

    46.-Dashboard-

--- app/Livewire/Admin/HomeComponent.php
+++ app/Livewire/Admin/HomeComponent.php
@@ -2,14 +2,24 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\User;
+use App\Models\Order;
+use App\Models\Product;
 use Livewire\Component;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 
 #[Layout('components.layouts.admin')]
+#[Title('Dashboard')]
 class HomeComponent extends Component
 {
     public function render()
     {
-        return view('livewire.admin.home-component');
+        $products_cnt = Product::query()->count();
+        $users_cnt = User::query()->count();
+        $orders_cnt = Order::query()->count();
+        $orders_total = Order::query()->sum('total');
+        return view('livewire.admin.home-component', compact('products_cnt', 'users_cnt',
+    'orders_cnt', 'orders_total'));
     }
 }
--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -32,7 +32,8 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('home') }}" target="_blanc">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('home') }}"
+                target="_blanc">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
@@ -84,35 +85,12 @@
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
 
-                        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                        <li class="nav-item dropdown no-arrow d-sm-none">
-                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-search fa-fw"></i>
-                            </a>
-                            <!-- Dropdown - Messages -->
-                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                                aria-labelledby="searchDropdown">
-                                <form class="form-inline mr-auto w-100 navbar-search">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-0 small"
-                                            placeholder="Search for..." aria-label="Search"
-                                            aria-describedby="basic-addon2">
-                                        <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button">
-                                                <i class="fas fa-search fa-sm"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </li>
-
                         <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->name }}</span>
+                                <span
+                                    class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->name }}</span>
                                 <img class="img-profile rounded-circle"
                                     src="{{ asset('assets/admin/img/undraw_profile.svg') }}">
                             </a>
@@ -138,7 +116,7 @@
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
 
-                    <h1 class="h3 mb-0 text-gray-800">{{ $title ?? 'Admin Page Title' }}</h1>
+                    <h1 class="h3 mb-3 text-gray-800">{{ $title ?? 'Admin Page Title' }}</h1>
 
                     {{ $slot }}
 
