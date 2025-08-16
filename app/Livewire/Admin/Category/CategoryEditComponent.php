d6479e0444bced52aa844438dbdfb4613b1a7dba
IvanTsumbek <maincumbek@gmail.com>
2025-08-16 21:40:20 +0300

    53.-Прикрепление-групп-фильтров-к-категории.-Часть-2-

--- app/Livewire/Admin/Category/CategoryEditComponent.php
+++ app/Livewire/Admin/Category/CategoryEditComponent.php
@@ -4,8 +4,11 @@ namespace App\Livewire\Admin\Category;
 
 use Livewire\Component;
 use App\Models\Category;
+use App\Models\FilterGroup;
 use Livewire\Attributes\Title;
 use Livewire\Attributes\Layout;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 #[Layout('components.layouts.admin')]
 #[Title('Edit Category')]
@@ -13,34 +16,68 @@ use Livewire\Attributes\Layout;
 class CategoryEditComponent extends Component
 {
     public Category $category;
+
     public string $title;
+
     public $parent_id = 0;
+
     public $id;
+
+    public array $selectedCategoryFilters = [];
 
     public function mount(Category $category)
     {
-    $this->category = $category;
-    $this->title = $category->title;
-    $this->parent_id = $category->parent_id;
-    $this->id = $category->id;
+        $this->category = $category;
+        $this->title = $category->title;
+        $this->parent_id = $category->parent_id;
+        $this->id = $category->id;
+        $this->selectedCategoryFilters = DB::table('category_filters')
+            ->where('category_id', '=', $this->category->id)
+            ->pluck('filter_group_id')
+            ->toArray();
+    }
 
-}
-
- public function save()
+    public function save()
     {
         $validated = $this->validate([
             'title' => 'required|max:255',
             'parent_id' => 'required|integer',
+            'selectedCategoryFilters.*' => 'numeric',
         ]);
 
-        $this->category->update($validated);
-        cache()->forget('categories_html');
-        session()->flash('success', 'Category updated successfully');
-        $this->redirectRoute('admin.categories.index', navigate:true);
+        try {
+            DB::beginTransaction();
+            $this->category->update($validated);
+            DB::table('category_filters')
+                ->where('category_id', '=', $this->category->id)
+                ->delete();
+            if (! empty($validated['selectedCategoryFilters'])) {
+                $data = [];
+                foreach ($validated['selectedCategoryFilters'] as $category_filter) {
+                    $data[] = [
+                        'category_id' => $this->category->id,
+                        'filter_group_id' => $category_filter,
+                    ];
+                }
+                DB::table('category_filters')->insert($data);
+            }
+            DB::commit();
+
+            cache()->forget('categories_html');
+            session()->flash('success', 'Category updated successfully');
+            $this->redirectRoute('admin.categories.index', navigate: true);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e->getMessage());
+            $this->js("toastr.error('Error updating category')");
+        }
     }
+
 
     public function render()
     {
-        return view('livewire.admin.category.category-edit-component');
+        $filter_groups = FilterGroup::all();
+
+        return view('livewire.admin.category.category-edit-component', compact('filter_groups'));
     }
 }
