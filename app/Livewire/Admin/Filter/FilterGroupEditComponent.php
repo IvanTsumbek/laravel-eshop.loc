42fea5ed4066bfe10cc1b4631d193148d6d84f0f
IvanTsumbek <maincumbek@gmail.com>
2025-08-25 19:49:10 +0300

    68.-Редактирование-группы-фильтров-

--- app/Livewire/Admin/Filter/FilterGroupEditComponent.php
+++ app/Livewire/Admin/Filter/FilterGroupEditComponent.php
@@ -3,11 +3,38 @@
 namespace App\Livewire\Admin\Filter;
 
 use Livewire\Component;
-
+use App\Models\FilterGroup;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Layout;
+#[Layout('components.layouts.admin')]
+#[Title('Edit Filter Group')]
 class FilterGroupEditComponent extends Component
 {
+    public FilterGroup $filterGroup;
+    public string $title;
+
+    public function mount(FilterGroup $filter_group)
+    {
+        $this->filterGroup = $filter_group;
+        $this->title = $filter_group->title;
+    }
+
+    public function save()
+    {
+        $validated = $this->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        $this->filterGroup->update($validated);
+
+        session()->flash('success', 'Filter group updated successfully');
+        $this->redirectRoute('admin.filter-groups.index', navigate: true);
+    }
+
+
     public function render()
     {
         return view('livewire.admin.filter.filter-group-edit-component');
     }
 }
+
