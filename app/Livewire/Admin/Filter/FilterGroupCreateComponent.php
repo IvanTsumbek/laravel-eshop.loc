cf5ee7a7e8c81a724b69a49d4306a5e2de6f912c
IvanTsumbek <maincumbek@gmail.com>
2025-08-25 19:30:36 +0300

    67.-Добавление-группы-фильтров-

--- app/Livewire/Admin/Filter/FilterGroupCreateComponent.php
+++ app/Livewire/Admin/Filter/FilterGroupCreateComponent.php
@@ -3,11 +3,30 @@
 namespace App\Livewire\Admin\Filter;
 
 use Livewire\Component;
-
+use App\Models\FilterGroup;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Layout;
+#[Layout('components.layouts.admin')]
+#[Title('Create Filter Group')]
 class FilterGroupCreateComponent extends Component
 {
+    public string $title;
+
+    public function save()
+    {
+        $validated = $this->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        FilterGroup::query()->create($validated);
+        session()->flash('success', 'Filter group created successfully');
+        $this->redirectRoute('admin.filter-groups.index', navigate: true);
+    }
+
+
     public function render()
     {
+
         return view('livewire.admin.filter.filter-group-create-component');
     }
 }
