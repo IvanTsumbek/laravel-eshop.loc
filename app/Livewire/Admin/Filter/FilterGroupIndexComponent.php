af23a43d64af1dfa153b4cfd23b3b40604dabdc4
IvanTsumbek <maincumbek@gmail.com>
2025-08-26 20:37:29 +0300

    69.-Удаление-групп-фильтров-

--- app/Livewire/Admin/Filter/FilterGroupIndexComponent.php
+++ app/Livewire/Admin/Filter/FilterGroupIndexComponent.php
@@ -2,16 +2,42 @@
 
 namespace App\Livewire\Admin\Filter;
 
-use App\Models\FilterGroup;
+use App\Models\Filter;
 use Livewire\Component;
+use App\Models\FilterGroup;
 use Livewire\Attributes\Title;
 use Livewire\Attributes\Layout;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 
 #[Layout('components.layouts.admin')]
 #[Title('Filter Groups')]
 class FilterGroupIndexComponent extends Component
 {
+    public function deleteFilterGroup(FilterGroup $filterGroup)
+    {
+        try {
+            DB::beginTransaction();
+            DB::table('filter_products')
+                ->where('filter_group_id', '=', $filterGroup->id)
+                ->delete();
+            Filter::query()->where('filter_group_id', '=', $filterGroup->id)
+                ->delete();
+            DB::table('category_filters')
+                ->where('filter_group_id', '=', $filterGroup->id)
+                ->delete();
+            $filterGroup->delete();
+            DB::commit();
+            $this->js("toastr.success('Filter group removed')");
+            return;
+        } catch (\Exception $e){
+            DB::rollback();
+            Log::error($e->getMessage());
+            $this->js("toastr.error('Error deliting filter group')");
+        }
+    }
+
     public function render()
     {
         $filter_groups = FilterGroup::all();
--- resources/views/livewire/admin/filter/filter-group-index-component.blade.php
+++ resources/views/livewire/admin/filter/filter-group-index-component.blade.php
@@ -1,6 +1,11 @@
 <div class="row">
     <div class="col-12 mb-4 position-relative">
 
+        <div class="update-loading" wire:loading>
+            <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
 
         <div class="card shadow mb-4">
             <div class="card-header py-3">
@@ -27,7 +32,7 @@
                                             <i class="fa-solid fa-pencil"></i>
                                         </a>
                                         <button class="btn btn-danger btn-circle"
-                                            wire:click="deleteFilterGroups({{ $filter_group->id }})"
+                                            wire:click="deleteFilterGroup({{ $filter_group->id }})"
                                             wire:confirm="Are you sure?"
                                             wire:loading.attr="disabled">
                                             <i class="fa-solid fa-trash"></i>
