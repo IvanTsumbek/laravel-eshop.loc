0f34919e801968d0c51cd52a4ad343be9c935f97
IvanTsumbek <maincumbek@gmail.com>
2025-08-27 19:56:33 +0300

    70.-Список-фильтров-

--- app/Livewire/Admin/Filter/FilterIndexComponent.php
+++ app/Livewire/Admin/Filter/FilterIndexComponent.php
@@ -3,11 +3,22 @@
 namespace App\Livewire\Admin\Filter;
 
 use Livewire\Component;
+use App\Models\Filter;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Layout;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Livewire\WithPagination;
 
+#[Layout('components.layouts.admin')]
+#[Title('Filters')]
 class FilterIndexComponent extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.admin.filter.filter-index-component');
+        $filters = Filter::query()->with('group')->paginate();
+        return view('livewire.admin.filter.filter-index-component', compact('filters'));
     }
 }
